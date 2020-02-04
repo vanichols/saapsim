3798b8fcd953c3966eae4e57fd5c6c286d0d937c
vanichols <vnichols@iastate.edu>
2020-02-04 10:36:37 -0600

    update saf_readapout function

--- R/readapout.R
+++ R/readapout.R
@@ -44,7 +44,7 @@ saf_readapout <- function(fold_dir) {
   }
 
   dat <- myraws %>%
-    dplyr::mutate(path = paste0(fold_dir, file)) %>%
+    dplyr::mutate(path = paste0(file)) %>%
     dplyr::mutate(res = path %>% purrr::map(helper_readrawoutfile)) %>%
     tidyr::unnest(cols = c(res)) %>%
     janitor::clean_names() %>%
