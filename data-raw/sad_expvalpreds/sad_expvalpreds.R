f3682cf66ceb2980e7be30e6042ea5fad5a195cd
vanichols <vnichols@iastate.edu>
2020-03-04 14:53:26 -0600

    add sad_expvalpreds data

---
+++ data-raw/sad_expvalpreds/sad_expvalpreds.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(readr)
+
+sad_tidysawyer
+
+exp <-
+  sad_tidysawyer %>%
+  mutate(data_type = "exp_vals",
+         year = as.character(year))
+
+preds <-
+  sad_tidysawyer %>%
+  group_by(site, rotation) %>%
+  nest() %>%
+  saf_fitNresp() %>%
+  filter(model == "QP") %>%
+  saf_predNresp() %>%
+  rename(yield_kgha = pred_kgha) %>%
+  mutate(year = "1999-2016",
+         data_type = "exp_pred") %>%
+  select(-model, -fit)
+
+sad_expvalpreds <- exp %>% bind_rows(preds)
+
+use_data(sad_expvalpreds, overwrite = TRUE)
+
