d7b11fa47814c36206d31ac91af888159c48e186
vanichols <vnichols@iastate.edu>
2020-03-23 08:41:26 -0500

    add sad_wea (weather data)

---
+++ data-raw/sad_wea/readme.txt
@@ -0,0 +1,3 @@
+Weather from the Iowa mesonet, 
+quality checked by the archontoulis lab, 
+provided by Mitch Baum Jan 2020
---
+++ data-raw/sad_wea/sad_wea.R
@@ -0,0 +1,63 @@
+library(tidyverse)
+library(janitor)
+#library(APSIM) --let's you read in met files! but puts them in a weird s4 format. I just stole their code.
+
+myd <- ("data-raw/sad_wea/")
+fl <- list.files(myd)
+fla <- fl[grepl(".met", fl)]
+
+
+
+for (i in 1:length(fla)) {
+
+  t.myp <- paste0(myd, fla[i])
+
+  t.site <- (fla[i] %>% str_split(., "_") %>% unlist())[1]
+
+  t.names <-
+    read_tsv(t.myp, skip = 2) %>%
+    slice(1) %>%
+    unlist(., use.names = FALSE) %>%
+    str_squish(.) %>%
+    str_split(., " ") %>%
+    unlist()
+
+  t.data <- read.table(
+    t.myp,
+    skip = 5,
+    header = FALSE,
+    col.names = t.names,
+    na.strings = c("?", "*"),
+    stringsAsFactors = FALSE
+  ) %>%
+    as_tibble() %>%
+    mutate(site = t.site)
+
+
+  if (i == 1) {
+    data <- t.data
+  } else {
+    data <- data %>% bind_rows(t.data)
+  }
+
+  i <- i + 1
+
+}
+
+
+
+#--make all sites only 4 characters
+dclean <-
+  data %>%
+  filter(site == "mcnay") %>%
+  mutate(site = str_sub(site, start = 1, end = 4))
+
+sad_wea <- dclean
+
+sad_wea %>% write_csv("data-raw/sad_wea/sad_wea.csv")
+use_data(sad_wea, overwrite = T)
+
+
+data <- read.table(f, skip = count - 1, header = FALSE, col.names = colNames,
+                   na.strings = c("?", "*"), stringsAsFactors = FALSE)
+names(data) <- colNames
