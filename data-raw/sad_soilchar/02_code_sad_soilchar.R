19035f1af3ab11025b0564a48b4855f40c414e2e
Unknown <vnichols@iastate.edu>
2020-03-26 13:21:37 -0500

    create sad_soilchar with wt and ssurgo data

--- data-raw/sad_soilchar/02_code_sad_soilchar.R
+++ data-raw/sad_soilchar/02_code_sad_soilchar.R
@@ -3,7 +3,7 @@ library(readxl)
 library(janitor)
 
 
-
+#--water table depth from FACTS paper
 swt <- read_excel("data-raw/sad_soilchar/archontoulis-et-al-2020-supplementary-table-S4.xlsx") %>%
   select(-location) %>%
   mutate(site = str_to_lower(site),
@@ -12,8 +12,14 @@ swt <- read_excel("data-raw/sad_soilchar/archontoulis-et-al-2020-supplementary-t
          site != "musc",
          !is.na(site))
 
-sad_soilchar <- swt
+#--iacsr, b horizon depth, and drainage class from ssurgo
+srgo <- read_csv("data-raw/sad_soilchar/td_ssurgo-vals.csv")
 
+
+
+sad_soilchar <- swt %>%
+  left_join(srgo) %>%
+  select(site, everything())
 
 sad_soilchar %>% write_csv("data-raw/sad_soilchar/sad_soilchar.csv")
 use_data(sad_soilchar, overwrite = TRUE)
