2162bb1798a6f34d654070d59f636c818c66a344
Unknown <vnichols@iastate.edu>
2020-03-26 09:20:01 -0500

    oops

--- data-raw/sad_siteinfo/code_sad_siteinfo.R
+++ data-raw/sad_siteinfo/code_sad_siteinfo.R
@@ -1,7 +1,11 @@
 library(tidyverse)
 library(readxl)
 
-sad_siteinfo <- read_excel("data-raw/sad_siteinfo/rd_siteinfo-facts.xlsx")
+sad_siteinfo <- read_excel("data-raw/sad_siteinfo/rd_siteinfo-facts.xlsx") %>%
+  mutate(site = str_sub(str_to_lower(site_name), 1, 4)) %>%
+  filter(site != "kell") %>%
+  rename(lon = long) %>%
+  select(site_name, site, lat, lon, drainage, irrigation)
 
 sad_siteinfo %>% write_csv("data-raw/sad_siteinfo/sad_siteinfo.csv")
 use_data(sad_siteinfo, overwrite = TRUE)
